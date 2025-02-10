8d879a4a505a5da3474e156fcabfc42902b9641e
Vishal <singhvishal9106@gmail.com>
2025-02-10 21:35:10 +0530

    cron test

--- app/api/daily-stats/route.ts
+++ app/api/daily-stats/route.ts
@@ -1,17 +1,19 @@
 import { NextResponse } from "next/server";
-import { dailyStats } from "@/lib/daily-stats";
-import connectToDatabase from "@/lib/db/mongodb";
+// import { dailyStats } from "@/lib/daily-stats";
+// import connectToDatabase from "@/lib/db/mongodb";
 
-export async function POST(request: Request) {
-  // Verify the request is coming from Vercel Cron
+export async function GET(request: Request) {
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    return new NextResponse("Unauthorized", {
+      status: 401,
+    });
   }
 
   try {
-    await connectToDatabase();
-    await dailyStats();
+    console.log("Cron job running...");
+    // await connectToDatabase();
+    // await dailyStats();
     return NextResponse.json(
       { message: "Daily stats job completed successfully" },
       { status: 200 },
